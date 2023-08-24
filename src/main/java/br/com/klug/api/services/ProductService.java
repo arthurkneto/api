a2b03030016145d918b16c17b24a29576ef086e4
Arthur <arthur.klug@bs.nttdata.com>
2023-08-24 09:12:50 -0300

    Categorias

--- src/main/java/br/com/klug/api/services/ProductService.java
+++ src/main/java/br/com/klug/api/services/ProductService.java
@@ -1,10 +1,13 @@
 package br.com.klug.api.services;
 
+import br.com.klug.api.model.Category;
 import br.com.klug.api.model.Product;
+import br.com.klug.api.repository.CategoryRepository;
 import br.com.klug.api.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,6 +15,9 @@ public class ProductService {
 
     @Autowired
     ProductRepository repository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
 
     public List<Product> getAll(){
         return repository.findAll();
@@ -22,6 +28,15 @@ public class ProductService {
     }
 
     public void save(Product product){
+
+        if( product.getCategories() != null ){
+            List<Category> categoriasAAdicionar = new ArrayList<>();
+            for( Category category : product.getCategories() ){
+                categoriasAAdicionar.add(categoryRepository.getReferenceById(category.getId()));
+            }
+            product.setCategories(categoriasAAdicionar);
+        }
+
         repository.save(product);
     }
 
